348b136fa866b02b009e479b8b597566a8dba9cb
Schneyder Garzon <Schneydergarzon@gmail.com>
2026-04-19 23:15:36 -0500

    Fix: Reconstruccion Login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -38,5 +38,19 @@ const login = async () => {
     <input v-model="email" type="email" placeholder="Email" class="w-full mb-2 p-2 border rounded" />
     <input v-model="password" type="password" placeholder="Contraseña" class="w-full mb-4 p-2 border rounded" />
     <button @click="login" class="bg-blue-600 text-white px-4 py-2 rounded w-full">Ingresar</button>
+  </div>  
+
+  <div class="max-w-md mx-auto mt-20 p-6 bg-white shadow rounded">
+    <h5 class="text-2xl font-bold mb-4">Usuarios de Prueba</h5>
+    <p class="font-bold">Admin</p>
+    <p >Email: admin@dentsoft.com</p>
+    <p>Contraseña: Admin123</p>
+    <p class="font-bold">Medico</p>
+    <p>Email: doctor@dentsoft.com</p>
+    <p>Contraseña: Doctor123</p>
+    <p class="font-bold">Asistente</p>
+    <p>Email: asistente@dentsoft.com</p>
+    <p>Contraseña: Asistente123</p>
   </div>
+
 </template>
